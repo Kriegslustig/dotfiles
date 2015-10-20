bfcd59eb2f33de79d7094aefaf5056254b973bac
Kriegslustig <github@ls7.ch>
2015-10-20 18:24:07 +0200

    Modify the dist.bash to symlink the files

--- dist.bash
+++ dist.bash
@@ -1,6 +1,9 @@
-cp -a .vim ~/
-cp .vimrc ~/
-cp .tmux.conf ~/
-cp .bashrc ~/
-cp .gitconfig ~/
+dir = $(pwd)/
+cd ~
+ln -s -a ${dir}.vim
+ln -s ${dir}.vimrc
+ln -s ${dir}.tmux.conf
+ln -s ${dir}.tmux.local.local
+ln -s ${dir}.bashrc
+ln -s ${dir}.gitconfig
 
