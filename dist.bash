a89cc75f4ad2387306d6589051fe88bc6915b2ab
Kriegslustig <github@ls7.ch>
2015-10-22 09:09:51 +0200

    Gotta work on my bash skills

--- dist.bash
+++ dist.bash
@@ -1,9 +1,9 @@
 dir=$(pwd)/
-cd ~ && \
-ln -fs -a ${dir}.vim && \
-ln -fs ${dir}.vimrc && \
-ln -fs ${dir}.tmux.conf && \
-ln -fs ${dir}.tmux.local && \
-ln -fs ${dir}.bashrc && \
+cd ~
+ln -fs ${dir}.vim
+ln -fs ${dir}.vimrc
+ln -fs ${dir}.tmux.conf
+ln -fs ${dir}.tmux.local
+ln -fs ${dir}.bashrc
 ln -fs ${dir}.gitconfig
 
