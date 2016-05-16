6369523ca4386ea7de081d751514216b3de3088a
Kriegslustig <kriegslustig@kriegslustig.me>
2016-05-16 16:09:50 +0200

    Fix a bug in the backup script

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tar c ~/TowerOwnCloud | zbackup --password-file ~/.backup_password backup /Volumes/p01504/backups/backups/towerowncloud-`date '+%Y-%m-%d'`
+tar c ~/TowerOwnCloud | /usr/local/bin/zbackup --password-file ~/.backup_password backup /Volumes/p01504/backups/backups/towerowncloud-`date '+%Y-%m-%d'`
 
